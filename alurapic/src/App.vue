e6b01f348fbd545b1b505fc6414c7e1770ad6035
hemersonGH <hemersonel@gmail.com>
2019-04-02 09:02:14 -0300

    Add folder alurapic

---
+++ alurapic/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1> {{ title }} </h1>
+
+    <li v-for="photo of photos" :key="photo">
+      <img :src="photo.url" :alt="photo.title" height="400" width="600">
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Alura Pic',
+      photos: [
+        {
+          url: 'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg',
+          title: 'Cat',
+        },
+        {
+          url: 'https://r.hswstatic.com/w_907/gif/tesla-cat.jpg',
+          title: 'Cat',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ alurapic/src/views/Test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="test">
+    <h1>This is an page of test</h1>
+  </div>
+</template>
